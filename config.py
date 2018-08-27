6a03b8e83c24d5c1aa091fdb8994a7bc0526eeb0
solaimurugan v <solaimurugan.v@gmail.com>
2018-08-27 14:22:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 # configure the dm text
 def generate_dm_text(name):
 	return '''Hey {}, It great to connect
-			with you on twitter'''.format(name)
+			with you on twitter !! \n\n I'm BOT. We are good in Twitter and Instagram BOT. \n\n Auto ReTweet, Direct Message and Auto instgram Post & Like. 😊😊  '''.format(name)
 
 scheduler_time = 15 #in minutes
 
 tw_username = "tamil_ai" #change this to yours
+
+#Hi ${name} , Thank you for following us ! \n\n Let us know if you need any help related to BOT service. \n\n We are good in Twitter and Instagram BOT. \n\n Auto ReTweet, Direct Message and Auto instgram Post & Like. 😊😊 `; 
